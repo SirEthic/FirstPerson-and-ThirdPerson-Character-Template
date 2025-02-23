13eed1c652c40542850830fdf8d054df4684047e
SirEthic <164008988+SirEthic@users.noreply.github.com>
2025-02-23 12:57:03 +0530

    Player Movement

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -7,15 +7,14 @@ const GRAVITY := 9.8
 
 @onready var animation_player: AnimationPlayer = $CharacterModel/Character/AnimationPlayer
 
-enum State {IDLE, WALK, RUN, CROUCH, STRAFE_LEFT, STRAFE_RIGHT}
+enum State {IDLE, WALK, RUN, JUMP, CROUCH}
 
 var anim_map : Dictionary = {
 	State.IDLE : "Idle",
 	State.WALK : "Walk",
 	State.RUN : "Run",
+	State.JUMP : "Idle",
 	State.CROUCH : "Crouch_Idle",
-	State.STRAFE_LEFT : "Strafe_Left",
-	State.STRAFE_RIGHT : "Strafe_Right",
 }
 
 var current_state = State.IDLE
--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,45 +1,86 @@
 class_name Player
 extends Character
 
-@export var sensitivity : float
-@export var jump_velocity : float
-@export var speed : float
+@export var sensitivity : float = 0.001
+@export var jump_velocity : float = 4.5
+@export var speed : float = 3.0
+@export var sprint_speed : float = 6.0
 
+@onready var character_model: Node3D = $CharacterModel
+@onready var first_person_camera: Camera3D = $Head/FirstPersonCamera
 @onready var head: Node3D = $Head
 @onready var third_person_camera: Camera3D = $Head/ThirdPersonCamera
+
+var is_first_person : bool = false
+var is_third_person : bool = true
+
+var current_speed = speed
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
-	super._physics_process(delta)
+	gravity(delta)
+	handle_input(delta)
+	handle_animations()
+	move_and_slide()
+
+func camera() -> void:
+	if is_third_person:
+		third_person_camera.current = false
+		third_person_camera.visible = false
+		
+		first_person_camera.current = true
+		first_person_camera.visible = true
+		
+		is_third_person = false
+		is_first_person = true
+	elif is_first_person:
+		third_person_camera.current = true
+		third_person_camera.visible = true
+		
+		first_person_camera.current = false
+		first_person_camera.visible = false
+		
+		is_third_person = true
+		is_first_person = false
 
 func _unhandled_input(event):
+	if Input.is_action_just_pressed("Camera"):
+		camera()
+	
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * sensitivity)
 		third_person_camera.rotate_x(-event.relative.y * sensitivity)
+		first_person_camera.rotate_x(-event.relative.y * sensitivity)
 		third_person_camera.rotation.x = clamp(third_person_camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		first_person_camera.rotation.x = clamp(third_person_camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
-func handle_input(_delta) -> void:
-	var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+func handle_input(delta) -> void:
 	
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = jump_velocity
+		current_state = State.JUMP
 	
-	if is_on_floor():
-		if direction.x > 0:
-			velocity.x = direction.x * speed
-			current_state = State.STRAFE_RIGHT
-		elif direction.x < 0:
-			velocity.x = direction.x * speed
-			current_state = State.STRAFE_LEFT
-		elif direction.z:
-			velocity.z = direction.z * speed
-			current_state = State.WALK
+	var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+		
+		character_model.rotation.y = lerp_angle(character_model.rotation.y, atan2(direction.x, direction.z), delta * 8.0)
+		
+		current_state = State.WALK
+		
+		if Input.is_action_pressed("Sprint"):
+			current_speed = sprint_speed
+			current_state = State.RUN
 		else:
-			velocity.x = move_toward(velocity.x , 0, speed)
-			velocity.z = move_toward(velocity.z , 0, speed)
-			current_state = State.IDLE
+			current_speed  = speed
+			current_state = State.WALK
+	else:
+		velocity.x = move_toward(velocity.x , 0, current_speed)
+		velocity.z = move_toward(velocity.z , 0, current_speed)
+		current_state = State.IDLE
 			
-	move_and_slide()
